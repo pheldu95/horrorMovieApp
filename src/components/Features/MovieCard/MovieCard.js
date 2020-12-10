15f2d537c071538aecfec586046fbdc755eda779
Duncan Phelps <phelps.duncan@gmail.com>
2020-12-10 14:34:44 -0700

    trying to make it so it shows the title when the mouse hovers over a poster

--- src/components/Features/MovieCard/MovieCard.js
+++ src/components/Features/MovieCard/MovieCard.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 import './MovieCard.css';
 import { Card, Image } from 'semantic-ui-react';
 
 const MovieCard = ({movie}) => {
-  
+  const[showTitle, setShowTitle] = useState(false);
+  function showDetails(){
+
+  }
   return (
-    <Image rounded src={'https://image.tmdb.org/t/p/w1280' + movie.poster_path} alt='movie poster' className='moviePoster' wrapped ui={false} />
-    // <Card className='movieCard'>
-    //   <Image src={'https://image.tmdb.org/t/p/w1280' + movie.poster_path} alt='movie poster' className='moviePoster' wrapped ui={false} />
-    //   <Card.Content>
-    //     <Card.Header>Matthew</Card.Header>
-    //     <Card.Meta>
-    //       <span className='date'>Joined in 2015</span>
-    //     </Card.Meta>
-    //     <Card.Description>
-    //         Matthew is a musician living in Nashville.
-    //     </Card.Description>
-    //   </Card.Content>
-    //   <Card.Content extra>
-    //     <a>
-    //       <Icon name='user' />
-    //       22 Friends
-    //     </a>
-    //   </Card.Content>
-    // </Card> 
+      <div className='container'>
+        {showTitle?
+          <img 
+            src={'https://image.tmdb.org/t/p/w1280' + movie.poster_path} 
+            alt='movie poster' className='moviePoster' 
+            onClick={() => showDetails()}
+            onMouseEnter={() => setShowTitle(true)}
+            onMouseLeave={() => setShowTitle(false)}
+          />
+        :
+          <img
+            src={'https://image.tmdb.org/t/p/w1280' + movie.poster_path}
+            alt='movie poster' className='showTitle'
+            onClick={() => showDetails()}
+            onMouseEnter={() => setShowTitle(true)}
+            onMouseLeave={() => setShowTitle(false)}
+          />
+          // <div
+          //   className = "showTitle"
+          //   onClick={() => showDetails()}
+          //   onMouseEnter={() => setShowTitle(true)}
+          //   onMouseLeave={() => setShowTitle(false)}
+          // style={{ backgroundImage: "url(" + "https://image.tmdb.org/t/p/w1280" + movie.poster_path + ")"}}
+          // >
+            
+          //</div>
+        } 
+      </div>
+    
   );
 }
 
