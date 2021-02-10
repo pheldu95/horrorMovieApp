02602a965cf71a383dd87677516e739570d094d5
Duncan Phelps <phelps.duncan@gmail.com>
2021-02-10 15:29:52 -0600

    changed the name of some tables and columns in db

--- server/routes/watchList.router.js
+++ server/routes/watchList.router.js
@@ -6,9 +6,9 @@ const pool = require('../modules/pool');
 router.get('/', (req, res) => {
     console.log(req.user)
     if(req.isAuthenticated()){
-        let queryText = `SELECT "movieId"
-                        FROM "watchList" 
-                        WHERE "userId" = ${req.user.id}
+        let queryText = `SELECT "movie_id"
+                        FROM "watch_list" 
+                        WHERE "user_id" = ${req.user.id}
                         ;`;
         pool.query(queryText).then((result) => {
             let movieIds = [];
@@ -29,13 +29,13 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     let item = req.body;
     console.log(item);
-    let queryText = `INSERT INTO "watchList" ("userId", "movieId")
+    let queryText = `INSERT INTO "watch_list" ("user_id", "movie_id")
                         VALUES ($1, $2)`;
     pool.query(queryText, [item.userId, item.movieId])
     .then(result =>{
         res.sendStatus(201);
     }).catch(error => {
-        console.log('error adding to watchList');
+        console.log('error adding to watch list');
         res.sendStatus(500);
     })
 });
