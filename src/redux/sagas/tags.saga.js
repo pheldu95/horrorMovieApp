e3dc4f8af5805d605f933766253b4703e4b28362
Duncan Phelps <phelps.duncan@gmail.com>
2021-03-11 23:02:04 -0600

    postTags in tags saga has two arrays ready to send to server. new tags and existing tags

--- src/redux/sagas/tags.saga.js
+++ src/redux/sagas/tags.saga.js
@@ -3,17 +3,35 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* postTags(action) {
     console.log(action.payload);
-    
+    //we will remove all existing tags from tagIds, and add them to a new array called tagsToUpCount
+    //for tagIds, we will send a post, for tagsToUpCount we will send a put to up their count
     let tagIds = action.payload.tagIds;
     let movieId = action.payload.movieId;
+    let tagsToUpCount = [];
     try {
         const response = yield axios({
             method: 'GET',
             url: `/api/tags/${movieId}`,
         });
-        console.log(response.data);
+        let existingTags = response.data.rows;
+        for(let i = 0; i < tagIds.length; i++){
+            let tagExists = false;
+            for(let existingTag of existingTags){
+                if(tagIds[i] === existingTag.id){
+                    tagExists = true;
+                }
+            }
+            if(tagExists){
+                tagsToUpCount.push(tagIds[i]);
+                tagIds.splice(i, 1);
+            }
+        }
+        console.log(tagsToUpCount);
+        console.log(tagIds);
         
-        //or maybe do a get request? to check for tags in db. and then do the posting and putting?
+        //now we do a post for new tags if there are any
+        // and a put for existing tags
+        
         // yield axios({
         //     method: 'POST',
         //     url: `/api/tags`,
